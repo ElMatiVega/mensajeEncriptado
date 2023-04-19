168ce1efb00de45c79fa8ce30aa885253abc111a
Matías Vega <elmativega3@gmail.com>
2023-04-19 17:20:58 -0300

    add function desencriptar

--- logicaEncriptación.js
+++ logicaEncriptación.js
@@ -1,14 +1,20 @@
 // document.addEventListener("DOMContentLoaded", encriptacion);
 const contenido = document.querySelector("#resultado");
+
 let boton = document.getElementById("botonEncriptar");
-// boton.onclick = encriptacion;
-boton.addEventListener('click',encriptacion)
+boton.addEventListener('click',encriptacion);
+
+let boton1= document.getElementById("botonDesencriptar");
+
+boton1.addEventListener('click', desencriptar );
 
 var html='';
+let texto = document.querySelector("textarea");
+
 
 function encriptacion() {
   
-  let texto = document.querySelector("textarea");
+  
   let textoAEncriptar = texto.value;
   
   let encriptado = "";
@@ -61,6 +67,38 @@ function encriptacion() {
 
 }
 
+
+function desencriptar(){
+  
+  let textoDesencriptar = texto.value;
+  
+  let desencriptado=textoDesencriptar.replace(/enter/gi, "e")
+  .replace(/imes/gi, "i")
+  .replace(/ai/gi, "a")
+  .replace(/ober/gi, "o")
+  .replace(/ufat/gi, "u");
+  console.log(desencriptado);
+  if (desencriptado.length > 0) {
+    html='';
+    html += `<textarea class="textResultado" id="textResultado" cols="25" rows="20" style=""> ${desencriptado}</textarea></br><button id="botonCopiar" onclick="copiarTextResultado()">Copiar</button>`;
+    
+  } else {
+    html='';
+    html += (` <img
+    class="imagenResultado"
+    src="imagenes/Muñeco.png"
+    alt="Imagen de busqueda"
+  />
+    <div>
+        <p class="pResultado" id="pResultado">Ningún mensaje fue encontrado</p>
+        <span class="spanResultado" >Ingresa el texto que desees encriptar o desencriptar</span>
+    </div>`);
+    
+  }
+  texto.value='';
+  contenido.innerHTML=html;
+  
+}
 
 function copiarTextResultado(){
   let respuesta= document.getElementById('textResultado');
